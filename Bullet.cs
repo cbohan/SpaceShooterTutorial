bc9f8d4a720dbfe9d9acb1bace5f3fd0cb1c87c4
unknown <connor.bohan@gmail.com>
2019-05-30 12:33:12 -0700

    Added player movement and shooting

---
+++ Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, Speed * Time.deltaTime, 0));
+
+        if (transform.position.y > 10) //If this bullet has gone off the screen.
+        {
+            //Destroy it.
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 10;
+
+    public GameObject BulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement.
+        float input = Input.GetAxisRaw("Horizontal");
+        transform.Translate(new Vector3(input * Time.deltaTime * Speed, 0, 0));
+
+        float maxXPosition = Camera.main.aspect * Camera.main.orthographicSize;
+
+        if (transform.position.x > maxXPosition)
+        {
+            transform.position = new Vector3(maxXPosition, transform.position.y, 0);
+        }
+        else if (transform.position.x < -maxXPosition)
+        {
+            transform.position = new Vector3(-maxXPosition, transform.position.y, 0);
+        }
+
+        //Shooting.
+        if (Input.GetButtonDown("Jump")) //If the player press Jump (which is the space button).
+        {
+            GameObject bullet = Instantiate(BulletPrefab); //Create new bullet.
+            bullet.transform.position = transform.position + new Vector3(0, .5f, 0); //Place the bullet at the position of the player.
+        }
+    }
+}
